281a9507954cc3e4c68639fd6572d201882e2c94
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-11-21 01:43:40 +0000

    added parseContract

--- src/verifyProofs.ts
+++ src/verifyProofs.ts
@@ -2,7 +2,7 @@ import { CID } from "multiformats/cid";
 import { create } from "ipfs-http-client";
 import { concat as uint8ArrayConcat } from 'uint8arrays/concat'
 import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
-import { DOMParser, XMLSerializer } from "xmldom";
+import { DOMParser, XMLSerializer } from "@xmldom/xmldom";
 /*
   @Dev: The functions that return Options might return Status.success or Status.failure
 */
@@ -13,7 +13,7 @@ export enum Status { success, failure };
  To get the result of the function, use options.data
  To print the error message, use options.error
 */
-export type Options = { error: string, status: Status, data: any }
+export type Options<T> = { error: string, status: Status, data: T }
 
 /*
   @Dev: The default ipfs configuration used by Ricardian Fabric
@@ -27,12 +27,35 @@ export const IPFSCONFIG: IPFSConfig = {
 export type IPFSConfig = { host: string, port: number, protocol: string }
 
 /*
+
+
+*/
+export type Contract = {
+    contracttype: string,
+    version: string,
+    created: string,
+    expires: string,
+    redirectto: string,
+    parenturl: string,
+    network: string,
+    issuer: string,
+    issuersignature: string,
+    participant: string,
+    participantsignature: string,
+    smartcontract: string,
+    erc20: string,
+    selectedWallet: string,
+    legalcontract: any
+}
+
+
+/*
  @Dev: Extract the CID from the path after Ricardian Fabric redirects.
  The CID will be appended as the last path in the url string.
  This function uses the browser API and returns options
 */
-export function getCIDfromPath(): Options {
-    let options: Options = { status: Status.success, error: "", data: "" }
+export function getCIDfromPath(): Options<string> {
+    let options: Options<string> = { status: Status.success, error: "", data: "" }
     // I split the url paths at /
     const pathArrays = window.location.pathname.split("/");
     const lastIndex = pathArrays.length - 1;
@@ -88,8 +111,8 @@ const all = async (source: any) => {
  Pass in the CID and the configuration.
 */
 
-export async function ipfsCat(CID: string, config: IPFSConfig): Promise<Options> {
-    const options: Options = { status: Status.success, data: "", error: "" }
+export async function ipfsCat(CID: string, config: IPFSConfig): Promise<Options<string>> {
+    const options: Options<string> = { status: Status.success, data: "", error: "" }
     try {
         const client = create(config);
         const data = uint8ArrayConcat(await all(client.cat(CID)))
@@ -106,21 +129,50 @@ export async function ipfsCat(CID: string, config: IPFSConfig): Promise<Options>
 @Dev: Parse the fetched contract to extract it's details
 */
 
-//TODO: maybe rename to verify contract and also pass in the parent to check for, to make sure this contract is not old, but the version the dev wants to use.
-export async function parseContract(html: string): Promise<Options> {
-    const options: Options = { status: Status.success, data: "", error: "" }
+export async function parseContract(html: string): Promise<Options<Contract>> {
+    const options: Options<Contract> = { status: Status.success, data: null, error: "" }
     try {
         const doc: Document = new DOMParser().parseFromString(html, "text/html");
-        const body = doc.getElementsByTagName("body");
-        if (body === null) {
-            throw "Invalid document.";
+
+        const page: Element = doc.getElementById("page");
+        if (page === null || page === undefined) {
+            throw new Error("Invalid document.");
+        }
+        const display: Element = doc.getElementById("contract-display");
+        const serializer = new XMLSerializer();
+        let display_innerHTML = "";
+        for (let i = 0; i < display.childNodes.length; i++) {
+            const node = display.childNodes[i];
+            display_innerHTML += serializer.serializeToString(node);
+        }
+        const contracttype = page.getAttribute("data-contracttype");
+
+        if (contracttype !== "fulfilled") {
+            throw new Error("Invalid contract type")
         }
 
-        for (const element of body) {
-            console.log("element");
+        options.data = {
+            contracttype: contracttype,
+            version: page.getAttribute("data-version"),
+            created: page.getAttribute("data-created"),
+            expires: page.getAttribute("data-expires"),
+            redirectto: page.getAttribute("data-redirectto"),
+            parenturl: page.getAttribute("data-parenturl"),
+            network: page.getAttribute("data-network"),
+            issuer: page.getAttribute("data-issuer"),
+            issuersignature: page.getAttribute("data-issuersignature"),
+            participant: page.getAttribute("data-participant"),
+            participantsignature: page.getAttribute("data-participantsignature"),
+            smartcontract: page.getAttribute("data-smartcontract"),
+            erc20: page.getAttribute("data-erc20"),
+            selectedWallet: page.getAttribute("metamask"),
+            legalcontract: display_innerHTML,
         }
-
-        // console.log(page.dataset);
+        const scripts = doc.getElementsByTagName("script")
+        if (scripts.length !== 0) {
+            // This should be only used for fulfilled contracts
+            throw new Error("Unknown scripts detected.")
+        }
     } catch (err: any) {
         options.error = err.message;
         options.status = Status.failure;
--- tests/verify.test.ts
+++ tests/verify.test.ts
@@ -1,16 +1,25 @@
-import { ipfsCat, IPFSCONFIG, Status, parseContract } from "../src";
+import { ipfsCat, IPFSCONFIG, Status, parseContract, } from "../src";
 
-const CID = "bafybeiaef5vqzlzmnqayunyo63gh56n72mc37r73pr7f2a5mu6ikasmojm";
+const CIDForFulfilled = "bafybeiaef5vqzlzmnqayunyo63gh56n72mc37r73pr7f2a5mu6ikasmojm";
+const CIDForAcceptable = "bafybeifklnfbvb36xt7or6cctsfs6c7llg66qqmlncajt4ojh4rj4ksxga";
+
+//TODO: maybe I get the parent from the smart contract, if I add web3 into this
+const parent = "https://bafybeifklnfbvb36xt7or6cctsfs6c7llg66qqmlncajt4ojh4rj4ksxga.ipfs.infura-ipfs.io/";
 
 test("fetches the contract using the content identifier", async () => {
-    const ipfsDataOptions = await ipfsCat(CID, IPFSCONFIG);
+    const ipfsDataOptions = await ipfsCat(CIDForFulfilled, IPFSCONFIG);
     expect(ipfsDataOptions.status).toBe(Status.success);
 })
 
-test("parses the fetched html", async () => {
-    const ipfsDataOptions = await ipfsCat(CID, IPFSCONFIG);
+test("parses the fetched contract", async () => {
+    const ipfsDataOptions = await ipfsCat(CIDForFulfilled, IPFSCONFIG);
     const contractOptions = await parseContract(ipfsDataOptions.data);
-    console.log(contractOptions);
     expect(contractOptions.status).toBe(Status.success);
+});
 
+test("parses the fetched contract and throws wrong contract type", async () => {
+    const ipfsDataOptions = await ipfsCat(CIDForAcceptable, IPFSCONFIG);
+    const contractOptions = await parseContract(ipfsDataOptions.data);
+    expect(contractOptions.status).toBe(Status.failure);
+    expect(contractOptions.error).toBe("Invalid contract type");
 });
